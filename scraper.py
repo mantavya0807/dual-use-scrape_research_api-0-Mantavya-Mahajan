e81ff91861e8d1c49376f2dfc47dc65ae5fa1ca6
mantavya0807 <mantavyamahajan@gmail.com>
2025-09-13 15:12:15 -0400

    part 2 complete

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import time
 import re
+import requests
 
 def scrape_page():
     url = 'https://openreview.net/group?id=NeurIPS.cc/2024/Workshop/SafeGenAi#tab-accept-oral'
@@ -26,14 +27,50 @@ def scrape_page():
                 name = link.text.strip()
                 homepage = base_url+link['href']
                 if name and name not in researchers:
-                    researchers[name] = homepage
-        researchers['Gilberto Leon'] = 'https://info.gilberto.codes/'
-        for name,homepage in researchers.items():
-            print(f'Name: {name}, Homepage: {homepage}')
+                    researchers[name] = {'profile_url': homepage, 'github': None}
+        researchers['Gilberto Leon'] = {'profile_url':'https://info.gilberto.codes/', 'github':None}
+        # for name,homepage in researchers.items():
+        #     print(f'Name: {name}, Homepage: {homepage}')
     finally:
         driver.quit()
-
     return researchers
 
+def find_github_links(researchers_data):
+    session = requests.session()
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    for name,data in researchers_data.items():
+        profile_url = data['profile_url']
+        if 'Gilberto' in profile_url:
+            pesonal_homepage_url = profile_url
+        else:
+            try:
+                response = session.get(profile_url, headers= headers, timeout=10)
+                response.raise_for_status
+                soup = BeautifulSoup(response.content, 'html.parser')
+                homepage_tag = soup.find('a', string='Homepage')
+                if not homepage_tag:
+                    print(f'no personal link for {name}')
+                    continue
+                personal_homepage_url = homepage_tag['href']
+
+            except requests.exceptions.RequestException as e:
+                print(f'could not process {name}')
+        try:
+            response = session.get(personal_homepage_url, headers=headers, timeout= 10)
+            response.raise_for_status()
+            soup=BeautifulSoup(response.content, 'html.parser')
+            github_link = soup.find('a', href = re.compile(r"github\.com"))
+            if github_link:
+                found_url = github_link['href']
+                researchers_data[name]['github'] = found_url
+                print(f'found {github_link} for {name}')
+            else:
+                print(f'no github for {name}')
+        except requests.exceptions.RequestException as e:
+            print(f'error in {name}: {e}')
+    
+    return researchers_data
+    
 if __name__ == "__main__":
-    scrape_page()
+    research_profiles = scrape_page()
+    fianl_data = find_github_links(research_profiles)
